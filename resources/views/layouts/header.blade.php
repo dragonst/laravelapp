6599beb01962822f75b492e0e9f2630849e318f3
dragonst <lilblingsod@gmail.com>
2017-11-12 14:59:09 +0200

    changed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -34,19 +34,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=no">
-    <meta name="description"
-          content="Jury.online enables users to make deals which, in case of dissatisfaction of any party, are considered by a panel of jurors delivering judgement">
-    <meta name="keywords"
-          content="blockchain, judgment, arbitrage, escrow, ethereum, dispute resolution">
-    <meta name="theme-color" content="#ffffff">
-
-    <meta property="og:title" content="Jury.online">
-    <meta property="og:description" content="Будущее решения споров">
-    <meta property="og:image"
-          content="https://jury.online/static/img/logo-shield.png">
-    <meta property="og:type" content="website">
-    <meta property="og:url"
-          content="https://jury.online/">
+  
 
     <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
