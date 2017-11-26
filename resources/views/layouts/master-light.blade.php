3d67380f9de23e9dd11ea783d7997a65445c2bd5
dragonst <lilblingsod@gmail.com>
2017-11-26 23:05:58 +0200

    change

--- resources/views/layouts/master-light.blade.php
+++ resources/views/layouts/master-light.blade.php
@@ -10,27 +10,14 @@
 
 
 
-      
+
 
 <link href="/css/app5ec8.css" rel="stylesheet"></head>
 
 <body data-code="">
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHDBVZ4"
-                height="0" width="0"
-                style="display:none;visibility:hidden"></iframe>
-    </noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    <!-- Google Tag Manager (noscript) -->
-    <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFHFT5X"
-                height="0" width="0"
-                style="display:none;visibility:hidden"></iframe>
-    </noscript>
-    <!-- End Google Tag Manager (noscript) -->
-
+    
 
 @yield('content')
 
