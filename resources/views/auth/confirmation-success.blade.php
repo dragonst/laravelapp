377e89f561c2909f7cf6dbd11dab348d53a5426c
dragonst <lilblingsod@gmail.com>
2017-11-26 22:32:43 +0200

    change

--- resources/views/auth/confirmation-success.blade.php
+++ resources/views/auth/confirmation-success.blade.php
@@ -1,8 +1,7 @@
 <html lang="en"><head>
 
-<title>Sign up — Jury.Online ICO Dashboard</title>
-<meta name="csrf-param" content="authenticity_token">
-<meta name="csrf-token" content="w4inU58+HEkvoeRtkVd39BBn0z3XTYYNgGy5Igmxu6fB0YzmvPm3aJKNI1qNQh17kmJzt3b9OWW2Xe5Zyu+YZg==">
+<title>ChainLancers</title>
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
